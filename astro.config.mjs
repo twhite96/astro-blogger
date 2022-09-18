e17dcdb86a6814729570ad043a9caeb955a87180
Scott Willsey <scott@saw66.com>
2022-09-18 00:15:43 -0700

    Fix footnotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import embeds from "astro-embed/integration";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
+import remarkGfm from "remark-gfm";
 import sitemap from "@astrojs/sitemap";
 import solid from "@astrojs/solid-js";
 
@@ -11,6 +12,10 @@ export default defineConfig({
   trailingSlash: "ignore",
   outDir: "dist",
   integrations: [embeds(), sitemap(), image(), mdx(), solid()],
+  markdown: {
+    remarkPlugins: [remarkGfm],
+    extendDefaultPlugins: true,
+  },
   vite: {
     ssr: {
       external: ["svgo"],
